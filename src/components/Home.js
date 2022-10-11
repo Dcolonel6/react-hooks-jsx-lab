501cc81eaca892c58d6901fe875ab177de038140
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-10-11 19:08:04 +0300

    finished assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  const styleObj = {
-    color : "firebrick"
-  };
+ 
   
   return ( 
-    <div id="home" style={styleObj}>
-      <h1>
-        {`${name} is a Web Developer from ${city}`}
+    <div id="home"  >
+      <h1 style= {{color: "firebrick"}}>
+        {name} is a Web Developer from {city}
       </h1>
     </div>
-    );
+  );
 }
 
 export default Home;
